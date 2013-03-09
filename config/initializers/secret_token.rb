49061a225e85c8d34747dd2dd9a2dca0f8c6ab23
sasaki takeru <sasaki.takeru@gmail.com>
2013-03-09 16:42:41 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4tuto::Application.config.secret_key_base = '84a6642702a3dd0897e965ef0f91b39cadfb39485a38bb9785ad748836371ae0b46eccfcc163bfdf83520dccac485d6ac946827a6140fdb9c7eb8fa2f556a2b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4tuto::Application.config.session_store :encrypted_cookie_store, key: '_rails4tuto_session'
